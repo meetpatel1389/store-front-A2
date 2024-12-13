0aa6363dd2d30349de74433544443f627c80b85c
meetpatel1389 <meetpatel1389@gmail.com>
2024-12-12 20:19:43 -0500

    updated images

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/best-buy-store.png" alt="best buy Store Logo">
       </a>
     </div>
     <button class="hamburger" @click="toggleNav">
